563fb8f10b9f0194239ba6abd5661820452daac0
“emilesherrott” <emilesherrott@gmail.com>
2025-12-03 11:40:30 +0000

    Improve create model method logic

--- models/Fruit.js
+++ models/Fruit.js
@@ -1,56 +1,60 @@
 const fruits = require("../fruits.json")
 
-class Fruit{
-    constructor ({genus, name, id, family, order, nutritions}) {
-        this.genus = genus
-        this.name = name
-        this.id = id
-        this.family = family
-        this.order = order
-        this.nutritions = nutritions
-    }
+class Fruit {
+  constructor({ genus, name, id, family, order, nutritions }) {
+    this.genus = genus
+    this.name = name
+    this.id = id
+    this.family = family
+    this.order = order
+    this.nutritions = nutritions
+  }
 
-    static showAll = () => {
-        return fruits.map(fruit => new Fruit(fruit))
-    }
+  static showAll = () => {
+    return fruits.map((fruit) => new Fruit(fruit))
+  }
 
-    static showOne = (name) => {
-        const fruit = fruits.filter(fruit => fruit.name.toLowerCase() == name)
-        if (fruit.length == 1){
-            return new Fruit(fruit[0])
-        } else {
-            throw Error("The fruit does not exist")
-        }
+  static showOne = (name) => {
+    const fruit = fruits.filter((fruit) => fruit.name.toLowerCase() == name)
+    if (fruit.length == 1) {
+      return new Fruit(fruit[0])
+    } else {
+      throw Error("The fruit does not exist")
     }
+  }
 
-    static create = (data) => {
-        const newFruit = data
-        newFruit["id"] = fruits.length + 1
-        fruits.push(newFruit)
-        return new Fruit(newFruit)
-
+  static create = (data) => {
+    const newFruit = data
+    const updatedFruit = fruits.find((fruit) => fruit.name.toLowerCase() == newFruit.name.toLowerCase())
+    if (!updatedFruit) {
+      newFruit["id"] = fruits.length + 1
+      fruits.push(newFruit)
+      return new Fruit(newFruit)
+    } else {
+        throw Error("Fruit already exists")
     }
+  }
 
-    update(data) {
-        const updatedFruit = fruits.find(fruit => fruit.name.toLowerCase() == this.name.toLowerCase())
-        console.log(updatedFruit)
-        if(updatedFruit) {
-            updatedFruit.name = data.name
-            return new Fruit(updatedFruit)
-        } else {
-            throw new Error("Fruit not found")
-        }
+  update(data) {
+    const updatedFruit = fruits.find((fruit) => fruit.name.toLowerCase() == this.name.toLowerCase())
+    console.log(updatedFruit)
+    if (updatedFruit) {
+      updatedFruit.name = data.name
+      return new Fruit(updatedFruit)
+    } else {
+      throw new Error("Fruit not found")
     }
+  }
 
-    destroy(){
-        const deletedFruit = fruits.find(fruit => fruit.name.toLowerCase() == this.name.toLowerCase())
-        if(deletedFruit){
-            const index = fruits.indexOf(deletedFruit)
-            fruits.splice(index, 1)
-        } else {
-            throw new Error("Fruit not found")
-        }
+  destroy() {
+    const deletedFruit = fruits.find((fruit) => fruit.name.toLowerCase() == this.name.toLowerCase())
+    if (deletedFruit) {
+      const index = fruits.indexOf(deletedFruit)
+      fruits.splice(index, 1)
+    } else {
+      throw new Error("Fruit not found")
     }
+  }
 }
 
 module.exports = Fruit
