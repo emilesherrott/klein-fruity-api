6cde09e9b5276752ca54181320eece3a2553017b
“emilesherrott” <emilesherrott@gmail.com>
2025-12-03 09:51:29 +0000

    add destroy route

---
+++ models/Fruit.js
@@ -0,0 +1,56 @@
+const fruits = require("../fruits.json")
+
+class Fruit{
+    constructor ({genus, name, id, family, order, nutritions}) {
+        this.genus = genus
+        this.name = name
+        this.id = id
+        this.family = family
+        this.order = order
+        this.nutritions = nutritions
+    }
+
+    static showAll = () => {
+        return fruits.map(fruit => new Fruit(fruit))
+    }
+
+    static showOne = (name) => {
+        const fruit = fruits.filter(fruit => fruit.name.toLowerCase() == name)
+        if (fruit.length == 1){
+            return new Fruit(fruit[0])
+        } else {
+            throw Error("The fruit does not exist")
+        }
+    }
+
+    static create = (data) => {
+        const newFruit = data
+        newFruit["id"] = fruits.length + 1
+        fruits.push(newFruit)
+        return new Fruit(newFruit)
+
+    }
+
+    update(data) {
+        const updatedFruit = fruits.find(fruit => fruit.name.toLowerCase() == this.name.toLowerCase())
+        console.log(updatedFruit)
+        if(updatedFruit) {
+            updatedFruit.name = data.name
+            return new Fruit(updatedFruit)
+        } else {
+            throw new Error("Fruit not found")
+        }
+    }
+
+    destroy(){
+        const deletedFruit = fruits.find(fruit => fruit.name.toLowerCase() == this.name.toLowerCase())
+        if(deletedFruit){
+            const index = fruits.indexOf(deletedFruit)
+            fruits.splice(index, 1)
+        } else {
+            throw new Error("Fruit not found")
+        }
+    }
+}
+
+module.exports = Fruit
